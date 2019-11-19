2639fbde42897099aff473d0f41da0f5f8c40b0f
Austin Witt <awitt@homeaway.com>
2019-11-19 15:26:35 -0600

    print out omor einfo

--- src/main/java/org/jenkinsci/plugins/workflow/libs/LibraryAdder.java
+++ src/main/java/org/jenkinsci/plugins/workflow/libs/LibraryAdder.java
@@ -89,7 +89,7 @@ import org.jenkinsci.plugins.workflow.flow.FlowCopier;
         listener.getLogger().println(
     			"LibraryVersions: [" + libraryVersions + "]" );
         
-        listener.getLogger().println( "Script: [" + execution.getScript() + "]" );
+        listener.getLogger().println( "Script: [" + execution.getScript() + "]; owner: [" + execution.getOwner() + "]" );
         
         List<Addition> additions = new ArrayList<>();
         LibrariesAction action = build.getAction(LibrariesAction.class);
